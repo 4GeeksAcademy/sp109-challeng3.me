7dfaa4c4a44c036aaf8fe965dd3833a069670c4d
NahuelPerrone85 <nahuelperrone85@gmail.com>
2025-08-01 18:52:23 +0000

    solve merge conflict

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
 					<Link to="/user/videojuego">
 						<button className="btn btn-primary">CRUD User/Videojuego</button>
 					</Link>
-					<Link to="/admin/dashboard">
+					<Link to="/admin/login">
 						<button className="btn btn-primary">Zona Administración</button>
 					</Link>
 					<Link to="/videojuego">
