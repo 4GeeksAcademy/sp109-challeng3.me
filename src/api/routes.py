864b52ee1006366eb749ce9146a15b8f79a90ab1
Jose Luis Vela <jlvela94@gmail.com>
2025-08-08 19:01:15 +0000

    eliminado caracter routes-py

--- src/api/routes.py
+++ src/api/routes.py
@@ -316,7 +316,7 @@ def add_users():
         username = body['username'],
         password = body['password'],
         email = body['email'],
-        level = 1©,
+        level = 1,
         points = 0,
         premium = False,
         is_active = True,
